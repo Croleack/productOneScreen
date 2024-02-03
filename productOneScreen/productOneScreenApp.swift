3dff9375dc0aca613beea87482e972695e8a10d8
Radik Khaziev <121854191+Croleack@users.noreply.github.com>
2024-02-03 15:23:33 +0500

    Initial Commit

---
+++ productOneScreen/productOneScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  productOneScreenApp.swift
+//  productOneScreen
+//
+//  Created by Enzhe Gaysina on 03.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct productOneScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
